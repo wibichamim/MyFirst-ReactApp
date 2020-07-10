8fe7a35fd23d9db722a82dde17d03effb15be443
wibichamim <wibi.mushodiq@students.amikom.ac.id>
2020-07-10 21:18:53 +0700

    Updates

---
+++ precache-manifest.20810f0c9ec1758b4e97a8476db907b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4444d2a3bc952e3c6ce9310e88cd8906",
+    "url": "/MyFirst-ReactApp/index.html"
+  },
+  {
+    "revision": "6bf702a94140bdf454af",
+    "url": "/MyFirst-ReactApp/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "6b8dc912edce1cb3e0ff",
+    "url": "/MyFirst-ReactApp/static/js/2.3e2e52e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyFirst-ReactApp/static/js/2.3e2e52e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf702a94140bdf454af",
+    "url": "/MyFirst-ReactApp/static/js/main.4fec7a98.chunk.js"
+  },
+  {
+    "revision": "c098f5a9a2b0a6c71281",
+    "url": "/MyFirst-ReactApp/static/js/runtime-main.79ae0425.js"
+  }
+]);
